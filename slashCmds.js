577d2a72b3ba7caf21450267da5539c66bf80c3a
michael <>
2026-09-05 10:54:16 -0400

    fix fix

--- slashCmds.js
+++ slashCmds.js
@@ -602,19 +602,21 @@ module.exports = {
 
             const domain = tokens.domain
             const cookie = tokens.cookie
-            const referrer = tokens.referrer
+            const referer = tokens.referer
 
             const res = await fetch(`${domain}/${name}`, {
                 method: "GET",
                 headers: {
-                    "Cookie": `${cookie}`,
+                    "Cookie": cookie,
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36",
                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                     "Accept-Language": "en-US,en;q=0.9",
-                    "Referer": `${referrer}`,
+                    "Referer": referer,
                 }
             });
             const html = await res.text();
+
+            console.log(html)
 
             const $ = cheerio.load(html);
             let links;

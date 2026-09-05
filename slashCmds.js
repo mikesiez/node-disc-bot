5e1fa044ca371be9ebef65f35598d66da4708f84
michael <>
2026-09-05 10:35:37 -0400

    new api support

--- slashCmds.js
+++ slashCmds.js
@@ -1,3 +1,4 @@
+import * as cheerio from "cheerio";
 const djs = require('discord.js');
 const djsV = require("@discordjs/voice");
 const { spawn } = require('child_process');
@@ -581,6 +582,48 @@ module.exports = {
                 await interaction.editReply(`error occured. dont do bad things | e:${e}`)
             }
             
+        }
+    },
+    getGame: {
+        name: "getGame",
+        description: "sail the seas",
+        options: [
+            {
+                name: "name",
+                description: "name as is EXACTLY",
+                type: 3,
+                required: true
+            },
+        ],
+        do : async function(/**@type {djs.ChatInputCommandInteraction} */interaction){
+            const name = interaction.options.getString("name");
+
+            const domain = tokens.domain
+            const cookie = tokens.cookie
+            const referrer = tokens.referrer
+
+            const res = await fetch(`${domain}/${name}`, {
+                method: "GET",
+                headers: {
+                    "Cookie": `${cookie}`,
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                    "Accept-Language": "en-US,en;q=0.9",
+                    "Referer": `${referrer}`,
+                }
+            });
+            const html = await res.text();
+
+            const $ = cheerio.load(html);
+            let links;
+            $("div").each((_, el) => {
+            if ($(el).text().trim() === "Gofile") {
+                const data = $(el).attr("data-links");
+                if (data) {
+                links = JSON.parse(data);
+                }
+            }
+            });
         }
     }
 }
